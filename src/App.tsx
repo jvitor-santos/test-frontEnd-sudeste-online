d63bf15fadc3b247d4877d339459f4d777008db7
jvitor-santos <jao_vitorsantos@hotmail.com>
2024-09-06 19:22:48 -0300

    Adicionado sessão abaixo do carrossel 'Ipsum dollor amet'

--- src/App.tsx
+++ src/App.tsx
@@ -161,6 +161,16 @@ export function App() {
             <p>Carregando ou sem resultados</p>
           )}
         </section>
+        <section className="flex w-[960px] flex-col pt-12">
+          <span className="text-4xl font-extrabold">Ipsum dollor amet</span>
+          <span className="pt-6 text-lg font-normal">
+            Lorem Ipsum é simplesmente uma simulação de texto da indústria
+            tipográfica e de impressos, e vem sendo utilizado desde o século
+            XVI, quando um impressor desconhecido pegou uma bandeja de tipos e
+            os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum
+            sobreviveu não só a cinco séculos.
+          </span>
+        </section>
       </main>
     </div>
   )
